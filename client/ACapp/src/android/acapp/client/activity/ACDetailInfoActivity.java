8dc1e3821becd64c7a2fbd561df5d82257553738
JingHongGang <jhg19900321@gmail.com>
2012-11-17 16:43:53 +0800

    create ACapp
    creat ACapp android client,add
    ACActegoryActivity,ACDetailInfoActivity,ACLoginActivity,ACMainActivity,ACShkeActivity.

---
+++ client/ACapp/src/android/acapp/client/activity/ACDetailInfoActivity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package android.acapp.client.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author trampboy
+ *
+ */
+public class ACDetailInfoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+
+}
---
+++ client/ACapp/src/android/acapp/client/activity/ACShakeActivity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package android.acapp.client.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author trampboy
+ *
+ */
+public class ACShakeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+			
+}

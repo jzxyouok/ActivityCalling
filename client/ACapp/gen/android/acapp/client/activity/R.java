23a14a1230bccc5e0df766b99f733816c9786d2e
JingHongGang <jhg19900321@gmail.com>
2012-11-19 23:44:21 +0800

    add function sina load and qq load

--- client/ACapp/gen/android/acapp/client/activity/R.java
+++ client/ACapp/gen/android/acapp/client/activity/R.java
@@ -15,8 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btn_login=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int btn_qqCancel=0x7f070003;
+        public static final int btn_qqLogin=0x7f070002;
+        public static final int btn_weiboCancel=0x7f070001;
+        public static final int btn_weiboLogin=0x7f070000;
+        public static final int menu_settings=0x7f070005;
+        public static final int tv_result=0x7f070004;
     }
     public static final class layout {
         public static final int activity_category=0x7f030000;
@@ -29,7 +33,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int btn_login=0x7f040004;
+        public static final int btn_qqCancel=0x7f040007;
+        public static final int btn_qqLogin=0x7f040006;
+        public static final int btn_weiboCancel=0x7f040005;
+        public static final int btn_weiboLogin=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_login=0x7f040003;
